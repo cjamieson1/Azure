c338f5a8fff3e5fddd563335f880e1f2584b7cda
Chris <51872429+cjamieson1@users.noreply.github.com>
2019-06-16 20:23:48 +0800

    test with 2 files

---
+++ azure/server.R
@@ -0,0 +1,37 @@
+#
+# This is the server logic of a Shiny web application. You can run the
+# application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(shinydashboard)
+library(shinydashboardPlus)
+library(googlesheets)
+library(BatchGetSymbols)
+library(plotly)
+library(shinythemes)
+library(shinyWidgets)
+library(glue)
+library(DT)
+library(dplyr)
+library(shinyalert)
+library(googledrive)
+library(stringr)
+library(mailR)
+library(lubridate)
+library(googlesheets)
+
+# Define server logic required to draw a histogram
+shinyServer(function(input, output) {
+    set.seed(122)
+    histdata <- rnorm(500)
+    
+    output$plot1 <- renderPlot({
+        data <- histdata[seq_len(input$slider)]
+        hist(data)
+    })
+})
---
+++ azure/ui.R
@@ -0,0 +1,29 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+
+# Define UI for application that draws a histogram
+shinyUI(
+    dashboardPage(
+    dashboardHeader(title = "Azure"),
+    dashboardSidebar(),
+    dashboardBody(
+        img(src = "Fremantle.png",height = "300px"),
+        # Boxes need to be put in a row (or column)
+        fluidRow(
+            box(plotOutput("plot1", height = 250)),
+            
+            box(
+                title = "Controls",
+                sliderInput("slider", "Number of observations:", 1, 100, 50)
+            )
+        )
+    )
+))
